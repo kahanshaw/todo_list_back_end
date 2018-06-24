76c28c394d2ee2dc8174c2e699ffb0cde9cd7819
Kalen Hanshaw <kahanshaw@cps.edu>
2018-06-24 16:28:50 +0000

    Add Edit and delete button

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,41 @@
 class TodoController < ApplicationController
+    def new
+    end
+    
+    def create
+        t = Todo.new
+        t.description = params[:description]
+        t.pomodoros = params[:pomodoros]
+        t.status = false
+        t.save
+        redirect_to('/todo/index')
+    end
+    
     def index
         @todos = Todo.all
     end
     
     def show
-        @todo = Todo.find([:id].to_i)
+        @todo = Todo.find(params[:id].to_i)
+    end
+    
+    def edit
+        @todo = Todo.find(params[:id].to_i)
+    end
+    
+    def delete
+        todo = Todo.find(params[:id].to_i)
+        todo.destroy
+        
+        redirect_to '/todo/index'
+    end
+    
+    def update
+        todo = Todo.find(params[:id].to_i)
+        todo.description = params[:description]
+        todo.pomodoros = params[:pomodoros]
+        todo.save
+        
+        redirect_to "/todo/show/#{todo.id}"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'todo/new', to: 'todo#new'
+  get 'todo/create', to: 'todo#create'
   get 'todo/index', to:'todo#index'
   get 'todo/show/:id', to: 'todo#show'
+  get 'todo/delete/:id', to: 'todo#delete'
+  get 'todo/edit/:id', to: 'todo#edit'
+  get 'todo/update/:id', to: 'todo#update'
 end
